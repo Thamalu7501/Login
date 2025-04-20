ea7e380c4053fec558df18c4cee2dc9a8b603d57
Thamalu7501 <thamalu7501@gmail.com>
2025-04-20 11:06:15 +0530

    Updating the changes made in Login.jsx and LoginStyle.css

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,24 +6,22 @@ function Login() {
         <div className="wrapper">
 
 <div className="title">
-  <span>Welcome back</span>
+  <span>Welcome</span>
 </div>
 <p className='title_para'>Please enter your details to sign in.</p>
 
 <form action="#">
   <div className="row">
     {/* <i className="fas fa-user"></i> */}
-    <input type="text" placeholder="Enter your email..." required />
+    <input type="text" placeholder="Enter your email" required />
   </div>
   <div className="row">
     {/* <i className="fas fa-lock"></i> */}
     <input type="password" placeholder="Password" required />
   </div>
-  <div className="pass"><a href="#">Forgot password?</a></div>
   <div className="row button">
     <input type="submit" value="Login" />
   </div>
-  <div className="signup-link">Not a member? <a href="#">Signup now</a></div>
 </form>
 </div>
       
