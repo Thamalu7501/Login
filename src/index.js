242a59feb4ca0de25c5395387e1879521f090ecc
Thamalu7501 <thamalu7501@gmail.com>
2025-04-23 07:22:49 +0530

    New Form Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Pages/Login';
+import OrderForm from './Pages/OrderForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path='OrderForm' element={<OrderForm />} />
     </Routes>
     </BrowserRouter>
 );
